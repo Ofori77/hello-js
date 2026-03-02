21d8f29d273111e73d96ea5f2ca9adbe8cdd95cc
Terrance Tenkorang <terranten37@gmail.com>
2026-03-02 13:57:08 +0000

    New addition

--- server.js
+++ server.js
@@ -4,26 +4,40 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-
-
 app.use(express.json());
 
-app.post('/la', (req, res) => {
-  console.log(req.body);
-  res.json( {mmmm: req.body });
-});
-
-app.get('/terry', (req, res) => {
-  res.send('Hello from Express!');
+app.use((req, res, next) => {
+  console.log(`New Request: ${req.method} exactly at ${req.url}`);
+  next();
 });
 
 app.get('/', (req, res) => {
-  res.send('Here is some data from the server!');
+  res.send('<h1>My Week 2 API!</h1>');
+});
+
+app.post('/user', (req, res) => {
+  const userName = req.body.name;
+  const userEmail = req.body.email;
+
+  if (!userName || !userEmail) {
+      return res.status(400).send("Error: Name and email are required" );
+  }
+
+  res.send(`Hello ${userName}!`);
+});
+
+
+app.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`User ${userId} profile`);
+});
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is successfully listening on port ${port}`);
 });
-});
+
 
 
 
